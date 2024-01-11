75e844ae5de42d73cf527cf1d499d38b5a5bb473
Anil-psychx <119948750+Anil-psychx@users.noreply.github.com>
2024-01-11 19:13:54 +0530

    Add files via upload

--- pdfview.js
+++ pdfview.js
@@ -1,70 +1,104 @@
-var url = 'Indian_Bank.pdf';
-var  pageNum = 1;
-var { pdfjsLib } = globalThis;
-pdfjsLib.GlobalWorkerOptions.workerSrc = "https://mozilla.github.io/pdf.js/build/pdf.worker.mjs";
+if (LeadID) {
+  var url = "Indian_Bank.pdf";
+  var pageNum = 1;
+  var { pdfjsLib } = globalThis;
+  pdfjsLib.GlobalWorkerOptions.workerSrc =
+    "https://mozilla.github.io/pdf.js/build/pdf.worker.mjs";
 
-var pdfDoc = null,
-  pageNum = 1,
-  pageRendering = false,
-  pageNumPending = null,
-  scale = 1,
-  canvas = document.getElementById('pdf-canvas'),
-  ctx = canvas.getContext('2d');
+  var pdfDoc = null,
+    pageNum = 1,
+    pageRendering = false,
+    pageNumPending = null,
+    scale = 1,
+    canvas = document.getElementById("pdf-canvas"),
+    ctx = canvas.getContext("2d");
 
+  function renderPage(num) {
+    pageRendering = true;
+    pdfDoc.getPage(num).then(function (page) {
+      var viewport = page.getViewport({ scale: scale });
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
 
-function renderPage(num) {
-pageRendering = true;
-pdfDoc.getPage(num).then(function(page) {
-  var viewport = page.getViewport({scale: scale});
-  canvas.height = viewport.height;
-  canvas.width = viewport.width;
+      var renderContext = {
+        canvasContext: ctx,
+        viewport: viewport,
+      };
+      var renderTask = page.render(renderContext);
 
-  var renderContext = {
-    canvasContext: ctx,
-    viewport: viewport
-  };
-  var renderTask = page.render(renderContext);
+      renderTask.promise.then(function () {
+        pageRendering = false;
+        if (pageNumPending !== null) {
+          renderPage(pageNumPending);
+          pageNumPending = null;
+        }
+      });
+    });
+    document.getElementById("page_num").textContent = num;
+  }
 
-  renderTask.promise.then(function() {
-    pageRendering = false;
-    if (pageNumPending !== null) {
-      renderPage(pageNumPending);
-      pageNumPending = null;
+  function queueRenderPage(num) {
+    if (pageRendering) {
+      pageNumPending = num;
+    } else {
+      renderPage(num);
     }
-  });
-});
-document.getElementById('page_num').textContent = num;
-}
+  }
 
+  function onPrevPage() {
+    if (pageNum <= 1) {
+      return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+  }
+  document.getElementById("prev").addEventListener("click", onPrevPage);
+  function onNextPage() {
+    if (pageNum >= pdfDoc.numPages) {
+      return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
+  }
+  document.getElementById("next").addEventListener("click", onNextPage);
 
-function queueRenderPage(num) {
-if (pageRendering) {
-  pageNumPending = num;
-} else {
-  renderPage(num);
-}
-}
+  function RenderPDF() {
+    pdfjsLib.getDocument(url).promise.then(function (pdfDoc_) {
+      pdfDoc = pdfDoc_;
+      document.getElementById("page_count").textContent = pdfDoc.numPages;
+      renderPage(pageNum);
+    });
+  }
 
+  getData();
 
-function onPrevPage() {
-if (pageNum <= 1) {
-  return;
+  async function getData() {
+    loader.style.display = "flex";
+    const response = await fetch(
+      `${baseUrl}banking-lead-pdf-s3url`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          lead_id: LeadID,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    const data = await response.json();
+    if (data?.result?.status.toLowerCase() === "true") {
+      url = data.result.s3_url;
+      RenderPDF();
+      const mq = window.matchMedia("(min-width: 481px)");
+      if (mq.matches) {
+        document.getElementById("pdf-canvas-iframe").setAttribute("src", url);
+      }
+      loader.style.display = "none";
+    } else {
+      LeadID = null;
+      loader.style.display = "none";
+      document.getElementById("content").innerHTML = empty;
+    }
+  }
 }
-pageNum--;
-queueRenderPage(pageNum);
-}
-document.getElementById('prev').addEventListener('click', onPrevPage);
-function onNextPage() {
-if (pageNum >= pdfDoc.numPages) {
-  return;
-}
-pageNum++;
-queueRenderPage(pageNum);
-}
-document.getElementById('next').addEventListener('click', onNextPage);
-
-pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
-pdfDoc = pdfDoc_;
-document.getElementById('page_count').textContent = pdfDoc.numPages;
-renderPage(pageNum);
-});
--- sign.js
+++ sign.js
@@ -1,4 +1,3 @@
-
 const canvas = document.getElementById("signature-canvas");
 canvas.height = canvas.offsetHeight;
 canvas.width = canvas.offsetWidth;
@@ -11,7 +10,7 @@ signaturePad.toDataURL("image/svg+xml"); // save image as SVG data url
 
 // Return svg string without converting to base64
 signaturePad.toSVG(); // "<svg...</svg>"
-signaturePad.toSVG({includeBackgroundColor: true}); 
+signaturePad.toSVG({ includeBackgroundColor: true });
 const data = signaturePad.toData();
 
 signaturePad.fromData(data);
@@ -30,23 +29,70 @@ signaturePad.off();
 // Rebinds all event handlers
 signaturePad.on();
 
+document
+  .getElementById("clear-signature-btn")
+  .addEventListener("click", function () {
+    signaturePad.clear();
+  });
+document
+  .getElementById("undo-signature-btn")
+  .addEventListener("click", function () {
+    var data = signaturePad.toData();
+    if (data) {
+      data.pop(); // remove the last dot or line
+      signaturePad.fromData(data);
+    }
+  });
+document
+  .getElementById("add-signature-btn")
+  .addEventListener("click", function () {
+    if (signaturePad.isEmpty()) {
+      return alert("Please provide a signature first.");
+    }
 
-document.getElementById('clear-signature-btn').addEventListener('click', function () {
-  signaturePad.clear();
-});
-document.getElementById('undo-signature-btn').addEventListener('click', function () {
-  var data = signaturePad.toData();
-  if (data) {
-    data.pop(); // remove the last dot or line
-    signaturePad.fromData(data);
+    loader.style.display = "flex";
+    var data = signaturePad.toDataURL();
+    UploadSign(data);
+  });
+async function createBlob(dataURL) {
+  var BASE64_MARKER = ";base64,";
+  if (dataURL.indexOf(BASE64_MARKER) == -1) {
+    var parts = dataURL.split(",");
+    var contentType = parts[0].split(":")[1];
+    var raw = decodeURIComponent(parts[1]);
+    return new Blob([raw], { type: contentType });
   }
-});
-document.getElementById('add-signature-btn').addEventListener('click', function () {
-  if (signaturePad.isEmpty()) {
-    return alert("Please provide a signature first.");
+  var parts = dataURL.split(BASE64_MARKER);
+  var contentType = parts[0].split(":")[1];
+  var raw = window.atob(parts[1]);
+  var rawLength = raw.length;
+
+  var uInt8Array = new Uint8Array(rawLength);
+
+  for (var i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i);
   }
-  
-  var data = signaturePad.toDataURL('image/png');
-  console.log(data);
-  window.open(data);
-});
+
+  return new Blob([uInt8Array], { type: contentType });
+}
+async function UploadSign(dataURL) {
+  const content = await createBlob(dataURL);
+  const formData = new FormData();
+  formData.append("lead_id", LeadID);
+  formData.append("signature_image", content);
+  const response = await fetch(
+    `${baseUrl}attach-signature?lead_id=${LeadID}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+  const data = await response.json();
+  if (data?.status?.toLowerCase() === "true") {
+    loader.style.display = "none";
+    alert("Signature updated successfully.");
+  } else {
+    loader.style.display = "none";
+    alert("Something went wrong. Please try agin.");
+  }
+}
